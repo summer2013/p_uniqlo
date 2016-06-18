6dd388f87dfd9da74f0cf104b7e3229c2d1f9cff
summer <shiny_mi@163.com>
2016-06-18 21:23:34 +0800

    add animation for page1

--- js/index.js
+++ js/index.js
@@ -1,6 +1,43 @@
-var swiper = new Swiper('.swiper-container', {
-	centeredSlides: true,
+//var swiper = new Swiper('.swiper-container', {
+	//centeredSlides: true,
 	//autoplay: 2500,
-	autoplayDisableOnInteraction: false
-});
+	//autoplayDisableOnInteraction: false
+//});
+
+
+$(function(){
+
+	//page1 animation
+	var tl = new TimelineLite();
+	tl.from($('.page1 .brand'),
+				   1,
+				   { top:"+=20px",
+					   opacity:"0",
+					   ease:Power0.easeIn })
+	.from($('.page1 main [class^=snow]'),
+				0.5,
+				{rotation:360,
+					repeat:-1})
+    .from($('.page1 main :not(.bow)'),
+				   0.5,
+				   {top:"+=20px",
+					   opacity:"0",
+					   ease:Power0.easeIn})
+	.from($('.page1 main .bow'),
+				   0.5,
+				   {width:0,
+					   opacity:"0",
+					   ease:Power0.easeIn})
+	.staggerFrom($('.page1 footer :not(.fire-cracker)'),
+						 1,
+						 {top:"+=20px",
+							 opacity:0},
+							 0.25)
+	.from($('.page1 footer .fire-cracker'),
+				0.5,
+			    {opacity:0,
+					repeat:3})
+
+
+})
 
