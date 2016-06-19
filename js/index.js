a731b8ae84b903de0d153452334e95cacd857c89
summer <shiny_mi@163.com>
2016-06-19 11:01:22 +0800

    finish page1 animation

--- js/index.js
+++ js/index.js
@@ -8,48 +8,20 @@
 $(function(){
 
 	//page1 animation
-	var tl = new TimelineMax();
+	var mainTeen = new TimelineLite();
+	var snowTeen = new TimelineMax({repeat:-1});
+	var fireCrackerTeen = new TimelineMax({repeat:3, delay:4, yoyo:true});
 
-	function getBrandAnimation() {
-		var brandTimeline = new TimelineMax();
-		brandTimeline.from($('.page1 .brand'), 1, { top:"+=20px", opacity:"0", ease:Power0.easeIn });
-		return brandTimeline;
-	};
+	mainTeen.from($('.page1 .brand'), 1, { top:"+=20px", opacity:"0", ease:Power0.easeIn })
+		.from($('.page1 main :not(.bow)'), 0.5, {top:"+=20px", opacity:"0", ease:Power0.easeIn})
+		.from($('.page1 main .bow'), 0.5, {width:0, opacity:"0", ease:Power0.easeIn}, 1.4)
+		.staggerFrom($('.page1 footer :not(.fire-cracker)'), 1, {top:"+=20px", opacity:0, delay: 0.5}, 0.2)
 
-	function getSnowAnimation() {
-		var snowTimeline = new TimelineMax({repeat:-1});
-		snowTimeline.from($('.page1 main [class^=snow]'), 0.5, {rotation:360});
-	};
+	snowTeen.from($('.page1 main [class^=snow]'), 0.5, {rotation:360});
+	fireCrackerTeen.from($('.page1 footer .fire-cracker'), 1, {opacity:0,ease:Power0.easeIn}, 0.2);
 
-	function getMainAnimation() {
-		var mainTimeline = new TimelineMax();
-		mainTimeline .from($('.page1 main :not(.bow)'), 0.5, {top:"+=20px", opacity:"0", ease:Power0.easeIn})
-			.from($('.page1 main .bow'), 0.5, {width:0, opacity:"0", ease:Power0.easeIn});
-		return mainTimeline;
-	};
-
-	function getFooterAnimation() {
-		var footerTimeline = new TimelineMax();
-		footerTimeline.staggerFrom($('.page1 footer :not(.fire-cracker)'), 1, {top:"+=20px", opacity:0, delay: 0.5}, 0.2);
-		return footerTimeline;
-	};
-
-	function getFireCrackerAnimation() {
-		var fcTimeline = new TimelineMax();
-		fcTimeline.from($('.page1 footer .fire-cracker'), 0.5, {opacity:0});
-		return fcTimeline;
-	};
-
-
-	tl.add(getBrandAnimation())
-		.add(getMainAnimation())
-		.add(getFooterAnimation())
-		.add(getFireCrackerAnimation())
-
-	getSnowAnimation();
 
 	TweenLite.set($(".page1"), {visibility:"visible"});
-
 
 })
 
